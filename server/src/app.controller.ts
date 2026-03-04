c58b981842936cc40b17266d71b3cdae424821c6
Dheeraj Thakur <dheerajthakur.dev@gmail.com>
2026-03-04 20:22:13 +0530

    Modify getHello to return request IP address

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import type { Request } from 'express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(
+    @Req() req: Request
+  ): string {
+    return `ip: ${req.ip}`;
   }
 }
